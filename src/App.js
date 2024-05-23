15a9e7eeb95a5e9a8d567a30ad11ab69bc3608b3
Pawel <pawel.werbowy@gmail.com>
2024-05-23 12:54:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios"; // used axios to make http requests
 import { Container, Row, Col, Button, Form, Alert } from "react-bootstrap";
 import WeatherDisplayCard from "./components/WeatherDisplayCard";
@@ -8,9 +8,15 @@ function App() {
   const [weather, setWeather] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const inputRef = useRef(null);
 
   // API_KEY is stored in.env file
   const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+  // useEffect hook to focus the input field when the component mounts
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   // function to get weather data from API
   const getWeather = async () => {
@@ -30,18 +36,28 @@ function App() {
     setLoading(false);
   };
 
+  // function to handle the enter key press to trigger the getNationality function
+  const pressEnter = (event) => {
+    if (event.keyCode === 13) {
+      getWeather();
+    }
+  };
+
   return (
     <Container className="mt-5">
-      <h1 className="text-center mb-4">Weather App</h1>
       <Row className="justify-content-center mb-3">
         <Col md={6}>
+          <h1 className="text-center mb-4">Weather App</h1>
           <Form>
-            <Form.Group>
+            <Form.Group controlId="formName">
               <Form.Control
+                ref={inputRef}
                 type="text"
                 placeholder="Enter City"
                 value={city}
                 onChange={(e) => setCity(e.target.value)}
+                onKeyDown={pressEnter}
+                disabled={loading}
               />
             </Form.Group>
 
