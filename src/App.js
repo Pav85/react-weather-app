d276508f8d8c5bf81667f10e1976e8219f8e58fd
Pawel <pawel.werbowy@gmail.com>
2024-05-23 10:03:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import axios from "axios";
 import { Container, Row, Col, Button, Form, Alert } from "react-bootstrap";
 
 function App() {
+  const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+  const getWeather = async (e) => {};
+
   return <Container className="mt-5"></Container>;
 }
 
